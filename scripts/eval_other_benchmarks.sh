105955fdc4f650ef20b6b6d6a45286b80bf6847a
冯思程 <fscdyx888@163.com>
2025-10-01 17:31:25 +0800

    web init

--- scripts/eval_other_benchmarks.sh
+++
@@ -1,6 +0,0 @@
-export LMUData="path/to/LMUData"
-export HF_TORCH_DTYPE=bfloat16
-
-torchrun --nproc-per-node=8 path/to/VLMEvalKit/run.py \
-    --data SEEDBench2_Plus SpatialEval VStarBench HRBench4K ChartQA_TEST MMStar \
-    --model your_model
--- scripts/merge_model.sh
+++
@@ -1,4 +0,0 @@
-python ../utils/model_merger_rm.py \
-  --local_dir path/to/actor \
-  --out_dir   path/to/merged_model \
-  --dtype bfloat16
